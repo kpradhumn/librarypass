a445ef324228a5f274a11a33df02faf5777a309d
TechieAditi <55248714+TechieAditi@users.noreply.github.com>
2020-04-18 17:32:02 +0530

    Added backend code

---
+++ app/src/main/java/Models/mStudent.java
@@ -0,0 +1,39 @@
+package Models;
+
+public class mStudent {
+    String nm;
+    String h;
+    String roll;
+    String br;
+    String strm;
+    String yr;
+    String phn;
+
+    public String getPhn() {
+        return phn;
+    }
+
+    public String getNm() {
+        return nm;
+    }
+
+    public String getH() {
+        return h;
+    }
+
+    public String getRoll() {
+        return roll;
+    }
+
+    public String getBr() {
+        return br;
+    }
+
+    public String getStrm() {
+        return strm;
+    }
+
+    public String getYr() {
+        return yr;
+    }
+}
